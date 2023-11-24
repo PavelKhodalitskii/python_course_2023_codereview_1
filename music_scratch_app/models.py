98a958f10ac3206d7b9be118e9ad581ac476a4c5
pleiada <dreamsobenatic00@mail.ru>
2023-11-24 20:24:14 +0800

    Initial commit

--- music_scratch_app/models.py
+++ music_scratch_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Guitar(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+    
+class GuitarData(models.Model):
+    class Types:
