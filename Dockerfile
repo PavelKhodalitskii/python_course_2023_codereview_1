1c3fc6b6598eba2adb1f73050231241b4beb290d
Pavel <dreamsobenatic00@mail.ru>
2023-12-15 14:35:35 +0800

    Tests added

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,5 @@ EXPOSE 8000
 
 CMD python3 manage.py makemigrations && \
     python3 manage.py migrate && \
+    python3 manage.py test && \
     python3 manage.py runserver 0.0.0.0:8000
--- muztorg_scratch_mainapp/scratch.py
+++ muztorg_scratch_mainapp/scratch.py
@@ -34,7 +34,7 @@ class Scratch:
             cls.__instance = super().__new__(cls)
         return cls.__instance
     
-    def get_photos(self, card_soup, product_detail, product):
+    def __get_photos(self, card_soup, product_detail, product):
         photos_divs = card_soup.find_all("div", class_="swiper-slide")
         product_photos = ProductPhotos(product_detail=product_detail)
         product_photos.save()
@@ -55,8 +55,6 @@ class Scratch:
                 photo.save()
 
                 if i == 0:
-                    print("IMAGE PATH: ", image_path, "SAVE PATH: ", save_path)
-
                     img_temp = NamedTemporaryFile(delete=True)
                     img_temp.write(image.content)
                     img_temp.flush()
@@ -72,31 +70,24 @@ class Scratch:
         product_detail.photos = product_photos
         product_detail.save()
 
-    def get_product_details(self, link, product):
+    def __get_product_details(self, link, product):
         card_response = get(link)
         card_soup = BeautifulSoup(card_response.text, "lxml")
         try:
             raiting = card_soup.find("div", class_="rating").find("span", class_="rating__value").text
         except:
             raise RuntimeError("Что-то пошло не так")
-        # product_info_div = card_soup.find("div", class_="container").find("div", class_="product-info ")
         product_info_div = card_soup.find("div", class_="product-page")
         description = product_info_div.find("div", class_="_description").text
-        # garanty = product_info_div.find(".product-info__i", first=True).find("div", first=True).text
-        # print(garanty)
 
         product_details = ProductDetails(product=product,
                                         raiting=raiting,
                                         about=description)
         product_details.save()
 
-        self.get_photos(card_soup, product_details, product)
-        # try:
-        #     self.get_photos(card_soup, product_details)
-        # except:
-        #     pass
+        self.__get_photos(card_soup, product_details, product)
 
-    def get_data_by_cat(self, cat, page):
+    def get_data_by_cat(self, cat, page, objects_amount=None):
         #Получаем url категории, формируем url страницы и получаем суп
         queryset = []
         url = self.urls[cat]
@@ -106,44 +97,46 @@ class Scratch:
         
         #Получаем все карточки товаров
         product_cards = soup.find_all('section', class_="product-thumbnail")
+        objects_parsed = 0
 
         for card in product_cards:
-            #Если не можем получить цену - запарсили что-то не то
-            try:
-                price = BeautifulSoup(str(card), 'lxml').section['data-price']
-            except:
-                continue
-
-            name_div = card.find("div", class_="title")
-            link_append = BeautifulSoup(f"{name_div.find('a')}", 'lxml').a['href']
-            name = name_div.find('a').text
-            link = self.base + link_append
-            
-            try:
-                #Пробуем получить продукт по имени
-                product = Product.objects.get(name=name)
-                queryset.append(product)
-                print("НАШЕЛ ТОВАР: ", product.name)
-                #Обновляем детали товара
-                # self.get_product_details(link, product)
-            except:
-                #Если такого нет создаем
-                cat_index = Product.ProductType.values.index(cat)
-                print(cat_index)
-                print(Product.ProductType.labels[cat_index])
-                product = Product(name=name,
-                            slug=slugify(name),
-                            price=price,
-                            link=link,
-                            category=Product.ProductType.choices[cat_index][0])
-                product.save()
-                print("СОЗДАЛ ТОВАР: ", name, price, link, sep=" ")
-
-                # Получаем детали товара
+            if objects_amount and objects_parsed < objects_amount:
+                #Если не можем получить цену - запарсили что-то не то
                 try:
-                    self.get_product_details(link, product)
-                except(RuntimeError):
+                    price = BeautifulSoup(str(card), 'lxml').section['data-price']
+                except:
                     continue
 
-                queryset.append(product)
+                name_div = card.find("div", class_="title")
+                link_append = BeautifulSoup(f"{name_div.find('a')}", 'lxml').a['href']
+                name = name_div.find('a').text
+                link = self.base + link_append
+                
+                try:
+                    #Пробуем получить продукт по имени
+                    product = Product.objects.get(name=name)
+                    queryset.append(product)
+                    print("Найден товар: ", product.name)
+                    objects_parsed += 1
+                except:
+                    #Если такого нет создаем
+                    cat_index = Product.ProductType.values.index(cat)
+                    print(Product.ProductType.labels[cat_index])
+                    product = Product(name=name,
+                                slug=slugify(name),
+                                price=price,
+                                link=link,
+                                category=Product.ProductType.choices[cat_index][0])
+                    
+                    product.save()
+                    objects_parsed += 1
+                    print("Создан товар: ", name, price, link, sep=" ")
+
+                    # Получаем детали товара
+                    try:
+                        self.__get_product_details(link, product)
+                    except(RuntimeError):
+                        continue
+
+                    queryset.append(product)
         return queryset
